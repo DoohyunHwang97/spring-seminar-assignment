30f3a7f14f26bfc9e0fb82525222ca7d2947a938
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-11-05 14:02:01 +0900

    feat(AdbimBatchServiceImpl): 멀티쓰레드 기능 추가

--- src/main/kotlin/admin/service/AdminBatchServiceImpl.kt
+++ src/main/kotlin/admin/service/AdminBatchServiceImpl.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service
 import java.util.concurrent.Executors
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
+import java.util.concurrent.Future
 
 @Service
 class AdminBatchServiceImpl(
@@ -17,61 +18,99 @@ class AdminBatchServiceImpl(
 
     override fun insertAlbums(albumInfos: List<BatchAlbumInfo>) {
         albumInfos.forEach { albumInfo ->
-            TransactionTemplate(txManager).executeWithoutResult {
-                var artistEntity = artistRepository.findByName(albumInfo.artist)
-                if (artistEntity == null) {
-                    artistEntity = ArtistEntity(name = albumInfo.artist)
-                    artistRepository.save(artistEntity)
-                }
+            val future = threads.submit {
+                TransactionTemplate(txManager).executeWithoutResult {
+                    var artistEntity = artistRepository.findByName(albumInfo.artist)
+                    if (artistEntity == null) {
+                        artistEntity = ArtistEntity(name = albumInfo.artist)
+                        artistRepository.save(artistEntity)
+                    }
 
-                var albumEntity = albumRepository.findByTitle(albumInfo.title)
-                if (albumEntity == null) {
-                    albumEntity = AlbumEntity(
-                            title = albumInfo.title,
-                            image = albumInfo.image,
-                            artist = artistEntity
-                    )
-                    albumRepository.save(albumEntity)
-                } else {
-                    albumEntity.title = albumInfo.title
-                    albumEntity.image = albumInfo.image
-                    albumEntity.artist = artistEntity
-                    albumEntity.songs.clear()
-                }
-
-                albumInfo.songs.forEach { songInfo ->
-                    var songEntities = songRepository.findAllByTitle(songInfo.title)
-                    var songEntity: SongEntity
-                    if (songEntities!!.isEmpty()) {
-                        songEntity = SongEntity(
-                                title = songInfo.title,
-                                duration = songInfo.duration,
-                                album = albumEntity
+                    var albumEntity = albumRepository.findByTitle(albumInfo.title)
+                    if (albumEntity == null) {
+                        albumEntity = AlbumEntity(
+                                title = albumInfo.title,
+                                image = albumInfo.image,
+                                artist = artistEntity
                         )
-                        songInfo.artists.forEach { artistName ->
-                            var songArtist = artistRepository.findByName(artistName)
-                            if (songArtist == null) {
-                                songArtist = ArtistEntity(name = artistName)
-                                artistRepository.save(songArtist)
-                            }
-                            songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist))
-                        }
-                        songRepository.save(songEntity)
-                        albumEntity.songs.add(songEntity)
+                        albumRepository.save(albumEntity)
+                    } else {
+                        albumEntity.title = albumInfo.title
+                        albumEntity.image = albumInfo.image
+                        albumEntity.artist = artistEntity
+                        albumEntity.songs.clear()
+                    }
 
-                    } else if (songEntities.size == 1) {
-                        songEntity = songEntities.first()
-                        if (songEntity.album.title == albumEntity.title) {
-                            songEntity.title = songInfo.title
-                            songEntity.duration = songInfo.duration
-                            songEntity.album = albumEntity
-                            songEntity.artists.clear()
-                        } else {
+                    albumInfo.songs.forEach { songInfo ->
+                        var songEntities = songRepository.findAllByTitle(songInfo.title)
+                        var songEntity: SongEntity
+                        if (songEntities!!.isEmpty()) {
                             songEntity = SongEntity(
                                     title = songInfo.title,
                                     duration = songInfo.duration,
                                     album = albumEntity
                             )
+                            songInfo.artists.forEach { artistName ->
+                                var songArtist = artistRepository.findByName(artistName)
+                                if (songArtist == null) {
+                                    songArtist = ArtistEntity(name = artistName)
+                                    artistRepository.save(songArtist)
+                                }
+                                songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist))
+                            }
+                            songRepository.save(songEntity)
+                            albumEntity.songs.add(songEntity)
+
+                        } else if (songEntities.size == 1) {
+                            songEntity = songEntities.first()
+                            if (songEntity.album.title == albumEntity.title) {
+                                songEntity.title = songInfo.title
+                                songEntity.duration = songInfo.duration
+                                songEntity.album = albumEntity
+                                songEntity.artists.clear()
+                            } else {
+                                songEntity = SongEntity(
+                                        title = songInfo.title,
+                                        duration = songInfo.duration,
+                                        album = albumEntity
+                                )
+                                songInfo.artists.forEach { artistName ->
+                                    var songArtist = artistRepository.findByName(artistName)
+                                    if (songArtist == null) {
+                                        songArtist = ArtistEntity(name = artistName)
+                                        artistRepository.save(songArtist!!)
+                                    }
+                                    songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist!!))
+                                }
+                                songRepository.save(songEntity)
+                                albumEntity.songs.add(songEntity)
+                            }
+
+
+                            songInfo.artists.forEach { artistName ->
+                                var songArtist = artistRepository.findByName(artistName)
+                                if (songArtist == null) {
+                                    songArtist = ArtistEntity(name = artistName)
+                                    artistRepository.save(songArtist!!)
+                                }
+                                songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist!!))
+                                albumEntity.songs.add(songEntity)
+                            }
+                        } else {
+                            if (songEntities.filter { it.album.title == albumEntity.title }.size == 1) {
+                                songEntity = songEntities.filter { it.album.title == albumEntity.title }.first()
+                                songEntity.title = songInfo.title
+                                songEntity.duration = songInfo.duration
+                                songEntity.album = albumEntity
+                                songEntity.artists.clear()
+                            } else {
+                                songEntity = SongEntity(
+                                        title = songInfo.title,
+                                        duration = songInfo.duration,
+                                        album = albumEntity
+                                )
+                            }
+
                             songInfo.artists.forEach { artistName ->
                                 var songArtist = artistRepository.findByName(artistName)
                                 if (songArtist == null) {
@@ -84,45 +123,10 @@ class AdminBatchServiceImpl(
                             albumEntity.songs.add(songEntity)
                         }
 
-
-                        songInfo.artists.forEach { artistName ->
-                            var songArtist = artistRepository.findByName(artistName)
-                            if (songArtist == null) {
-                                songArtist = ArtistEntity(name = artistName)
-                                artistRepository.save(songArtist!!)
-                            }
-                            songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist!!))
-                            albumEntity.songs.add(songEntity)
-                        }
-                    } else {
-                        if (songEntities.filter { it.album.title == albumEntity.title }.size == 1) {
-                            songEntity = songEntities.filter { it.album.title == albumEntity.title }.first()
-                            songEntity.title = songInfo.title
-                            songEntity.duration = songInfo.duration
-                            songEntity.album = albumEntity
-                            songEntity.artists.clear()
-                        } else {
-                            songEntity = SongEntity(
-                                    title = songInfo.title,
-                                    duration = songInfo.duration,
-                                    album = albumEntity
-                            )
-                        }
-
-                        songInfo.artists.forEach { artistName ->
-                            var songArtist = artistRepository.findByName(artistName)
-                            if (songArtist == null) {
-                                songArtist = ArtistEntity(name = artistName)
-                                artistRepository.save(songArtist!!)
-                            }
-                            songEntity.artists.add(SongArtistEntity(song = songEntity, artist = songArtist!!))
-                        }
-                        songRepository.save(songEntity)
-                        albumEntity.songs.add(songEntity)
                     }
-
                 }
             }
+            future.get()
         }
     }
 }
